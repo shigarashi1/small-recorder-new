528af317dd1de26802e70593891104203bc6be49
S.Igarashi <50ra4.dev.dev@gmail.com>
2020-03-20 02:23:03 +0900

    ADD: alias

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,15 @@
-const path = require('path');
+/* eslint-disable */
+const path = require("path");
 
 module.exports = function override(config) {
   config.resolve = {
     ...config.resolve,
     alias: {
-      '@Components': path.resolve(__dirname, 'src/presentation/components'),
-      '@': path.resolve(__dirname, 'src'),
-    },
+      "@Components": path.resolve(__dirname, "src/presentation/components"),
+      "@Events": path.resolve(__dirname, "src/presentation/events"),
+      "@DomainModels": path.resolve(__dirname, "src/domain/models"),
+      "@": path.resolve(__dirname, "src")
+    }
   };
   return config;
 };
