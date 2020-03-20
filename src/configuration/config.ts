353ff8936eda4bd960968dacb9fea0582a51bd57
S.Igarashi <50ra4.dev.dev@gmail.com>
2020-03-20 20:40:44 +0900

    FIXED: configration/*

--- src/configuration/config.ts
+++ src/configuration/config.ts
@@ -1,1 +1,17 @@
-export const config = {};
+import { firebaseConfig } from './private/firebase';
+import { sentryConfig } from './private/sentry';
+
+const isProduction = process.env.NODE_ENV === 'production';
+const isStaging = process.env.REACT_APP_STAGING_FLAG === '1';
+const isTest = process.env.NODE_ENV === 'test';
+
+const config = {
+  isProduction: isProduction && !isStaging,
+  isStaging,
+  isTest,
+  isDev: !isProduction && !isStaging,
+  firebase: isProduction ? (!isStaging ? firebaseConfig.production : firebaseConfig.staging) : firebaseConfig.develop,
+  sentry: isProduction ? (!isStaging ? sentryConfig.production : sentryConfig.staging) : sentryConfig.develop,
+};
+
+export default config;
---
+++ src/configuration/private/readme.md
@@ -0,0 +1,4 @@
+### configulation/private
+
+ここには設定ファイルを置く。
+機密情報を含む場合、`.gitignore`に書くのを忘れずに
