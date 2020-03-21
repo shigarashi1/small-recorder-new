a96f5412357b0b585209a53b3b853c7635f6519f
S.Igarashi <50ra4.dev.dev@gmail.com>
2020-03-21 11:52:13 +0900

    FIXED: types

--- src/presentation/types/index.ts
+++ src/presentation/types/index.ts
@@ -3,4 +3,4 @@ export type TI18nObj = {
   [ELangCode.Jp]: string;
   [ELangCode.En]?: string;
 };
-export type TLangCode = keyof TI18nObj;
+export type TLangCode = typeof ELangCode[keyof typeof ELangCode];
