903a0baeb7477d15b450db02605b9944c3959fc6
S.Igarashi <50ra4.dev.dev@gmail.com>
2020-04-05 18:53:25 +0900

    ADD: logger

--- src/application/eventListener/ui/dialogs.ts
+++ src/application/eventListener/ui/dialogs.ts
@@ -1,16 +1,18 @@
 import { AnyAction } from 'typescript-fsa';
 import { Epic, combineEpics } from 'redux-observable';
 import { ofAction } from 'typescript-fsa-redux-observable-of-action';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { AppState } from '@/store';
 import { errorActions } from '@/root/error-actions';
 import { errorDialogModule } from '@/store/ui/error-dialog';
 import { WrapAction } from '@/library/redux-observable';
+import { Logger } from '@/library/models/logger';
 
 // error dialog Epics
 const throwError: Epic<AnyAction, WrapAction<typeof errorDialogModule.actions.show>, AppState> = (action$, store) =>
   action$.pipe(
     ofAction(errorActions.throwError),
+    tap(({ payload }) => Logger.warn(payload)),
     map(({ payload }) => {
       const { code, errorCode, message } = payload;
       return errorDialogModule.actions.show({ code, errorCode, message });
